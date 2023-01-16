a4c398e8cc4031529546023032f5f91a37c7c843
parkshub <AndrewJHPark@protonmail.com>
2023-01-16 15:58:23 +0900

    deployment

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,14 @@ app.use(cors());
 app.use("/api/user", userRouter)
 app.use("/api/event", eventRouter)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html")))
+} else {
+    app.get("/", (req, res) => res.send("Please set to production"))
+}
+
 
 
 app.listen(process.env.PORT || 5000, () => {
